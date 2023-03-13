ddbab629f418c1472ef35efb3b35aa32b73b6e3d
ggentile <giannikleiss4@gmail.com>
2023-03-13 11:57:08 -0300

    ajuste correção senha django exposta

--- projeto_farmacia/settings.py
+++ projeto_farmacia/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=62y@f@n3w*4=3z@ac72drrslj6!ykoxrhh@wio4$o(lxg)xcr'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
